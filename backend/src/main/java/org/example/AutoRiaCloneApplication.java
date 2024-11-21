a5b23b58050b8c206ec5f0be3329961e9c0affb4
Артём Рассветный <artemius.morningstar@gmail.com>
2024-11-21 13:38:28 +0200

    Initial commit

---
+++ backend/src/main/java/org/example/AutoRiaCloneApplication.java
@@ -0,0 +1,11 @@
+package org.example.auto_ria_clone.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRiaCloneApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoRiaCloneApplication.class, args);
+    }
+}
